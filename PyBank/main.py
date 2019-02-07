b7528f27211d65b558794203ed6340a23cdf647e
alobodanov <artem.lobodanov@gmail.com>
2019-02-06 21:08:38 -0500

    part1 almost done but missing average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,25 +1,41 @@
 import os
 import csv
 
-cereal_csv = os.path.join("./", "budget_data.csv")
-numberMonth = 0;
-netTotal = 0;
+budget_data_csv = os.path.join("./", "budget_data.csv")
+month_total = 0
+monthy_change = []
+data = []
+net_total = 0
+average_profit = 0
+original_profit = 0
+greatest_value = 0
+greatest_date_on = 0
+lowest_value = 0
+lowest_date_on = 0
 
-with open(cereal_csv, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(budget_data_csv, newline="") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    csv_header = next(csv_file)
 
-    csv_header = next(csvfile)
-    print(f"Header: {csv_header}")
-
-    for row in csvreader:
+    for row in csv_reader:
         if row:
-            numberMonth = numberMonth + 1;
-            netTotal = netTotal + int(row[1]);
+            data.append(row[0])
+            month_total = month_total + 1
+            net_total = net_total + int(row[1])
 
-    print('Financial Analysis');
-    print('----------------------------');
-    print(f'Total Month: {numberMonth - 1}');
-    print(f'Total: ${netTotal}');
-    print(f'Average Change: {netTotal / (numberMonth - 1)}');
-    print(f'Greatest Increase in Profits: {numberMonth - 1}');
-    print(f'Greatest Decrease in Profits: {numberMonth - 1}');
+            original_profit = int(row[1])
+            change_profit = original_profit - average_profit
+            average_profit = original_profit
+
+            monthy_change.append(original_profit)
+
+            greatest_value = max(monthy_change)
+            lowest_value = min(monthy_change)
+
+    print('Financial Analysis')
+    print('----------------------------')
+    print(f'Total Month: {month_total}')
+    print(f'Total: ${net_total}')
+    print(f'Average Change: ${( int(greatest_value) - int(lowest_value )) / int(month_total) - 2}')
+    print(f'Greatest Increase in Profits: {data[monthy_change.index(greatest_value)]} (${greatest_value})')
+    print(f'Greatest Decrease in Profits: {data[monthy_change.index(lowest_value)]} (${lowest_value})')
