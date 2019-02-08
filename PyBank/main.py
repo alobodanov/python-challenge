60b5d21f9ed018a4713f5752257140f87960dcb2
alobodanov <artem.lobodanov@gmail.com>
2019-02-08 11:35:36 -0500

    final submition

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,55 +1,44 @@
 import os
 import csv
 
-budget_data_csv = os.path.join("./", "budget_data.csv")
-budget_data_result_csv = os.path.join("./", "budget_data_result.csv")
+budget_data_csv = os.path.join('./', 'budget_data.csv')
+budget_data_result_csv = os.path.join('./', 'budget_data_result.csv')
 
-month_total = 0
-month_change = []
-data = []
-net_total = 0
-average_profit = 0
-original_profit = 0
-greatest_value = 0
-greatest_date_on = 0
-lowest_value = 0
-lowest_date_on = 0
+total_month = []
+total_change = []
+monthly_change = []
 
-with open(budget_data_csv, newline="") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
+with open(budget_data_csv, newline='') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
     csv_header = next(csv_file)
 
     for row in csv_reader:
-        if row:
-            data.append(row[0])
-            month_total = month_total + 1
-            net_total = net_total + int(row[1])
+        total_month.append(row[0])
+        total_change.append(int(row[1]))
 
-            original_profit = int(row[1])
-            change_profit = original_profit - average_profit
-            average_profit = original_profit
-
-            month_change.append(original_profit)
-
-            greatest_value = max(month_change)
-            lowest_value = min(month_change)
+    for i in range(len(total_change) - 1):
+        monthly_change.append(total_change[i + 1] - total_change[i])
 
     print('Financial Analysis')
     print('----------------------------')
-    print(f'Total Month: {month_total}')
-    print(f'Total: ${net_total}')
-    print(f'Average Change: ${( int(greatest_value) - int(lowest_value )) / int(month_total) - 2}')
-    print(f'Greatest Increase in Profits: {data[month_change.index(greatest_value)]} (${greatest_value})')
-    print(f'Greatest Decrease in Profits: {data[month_change.index(lowest_value)]} (${lowest_value})')
+    print(f'Total Month: {len(total_month)}')
+    print(f'Total: ${sum(total_change)}')
+    print(f'Average Change: ${sum(monthly_change) / len(monthly_change):.2f}')
+    print(f'Greatest Increase in Profits: '
+          f'{total_month[monthly_change.index(max(monthly_change)) + 1]} (${max(monthly_change)})')
+    print(f'Greatest Decrease in Profits: '
+          f'{total_month[monthly_change.index(min(monthly_change)) + 1]} (${min(monthly_change)})')
 
 csv_file.close()
 
-file = open(budget_data_result_csv, "w")
+file = open(budget_data_result_csv, 'w')
 file.write('Financial Analysis\n')
 file.write('----------------------------\n')
-file.write(f'Total Month: {month_total}\n')
-file.write(f'Total: ${net_total}\n')
-file.write(f'Average Change: ${( int(greatest_value) - int(lowest_value )) / int(month_total) - 2}\n')
-file.write(f'Greatest Increase in Profits: {data[month_change.index(greatest_value)]} (${greatest_value})\n')
-file.write(f'Greatest Decrease in Profits: {data[month_change.index(lowest_value)]} (${lowest_value})\n')
+file.write(f'Total Month: {len(total_month)}\n')
+file.write(f'Total: ${sum(total_change)}\n')
+file.write(f'Average Change: ${sum(monthly_change) / len(monthly_change):.2f}\n')
+file.write(f'Greatest Increase in Profits: '
+           f'{total_month[monthly_change.index(max(monthly_change)) + 1]} (${max(monthly_change)})\n')
+file.write(f'Greatest Decrease in Profits: '
+           f'{total_month[monthly_change.index(min(monthly_change)) + 1]} (${min(monthly_change)})\n')
 file.close()
